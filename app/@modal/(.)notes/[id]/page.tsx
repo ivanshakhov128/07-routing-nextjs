061e11f8b3a4ade7ebf37fa99b46adad32072228
Ivan Shakhov <ivanshahov04@gmail.com>
2025-12-18 22:04:07 +0200

    rename_2

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview";
+import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview.client";
 
 export default function NoteModal({ params }: { params: { id: string } }) {
   return <NotePreview noteId={params.id} />;
